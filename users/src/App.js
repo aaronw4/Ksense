f6fb6d7cd399cd416b409787935f15cebbf6a81c
Aaron Williams <aaronrw4@yahoo.com>
2021-11-11 18:31:58 -0800

    Created Routes

--- users/src/App.js
+++ users/src/App.js
@@ -1,10 +1,20 @@
+import React, { useState } from 'react';
+import {Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Posts from './Posts';
 import './App.css';
 
 function App() {
+  const [id, setID] = useState('')
+
   return (
-    <div className="App">
-    </div>
-  );
+      <div className='App'>
+          <Routes>
+            <Route exact path='/' element={<Home setID={setID}/>}/>
+            <Route path='/posts' element={<Posts id={id}/>}/>
+          </Routes>
+      </div>
+  )
 }
 
 export default App;
